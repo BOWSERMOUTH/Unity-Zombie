258042f1e05ff79b2200a32bbd48fd49d54e8d88
Jeffrey Meldgin <Jeff@Jeff-Medglins-JockBook-Bro.local>
2016-07-06 16:04:03 -0700

    initial commit

---
+++ Assets/CubeSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello, World!");
+		Debug.Log ("I am dead inside");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("times since last frame" + Time.deltaTime);
+	}
+
+
+}
---
+++ Assets/SpawnCube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCube : MonoBehaviour {
+
+	public GameObject myCube;
+	public GameObject myZombiePrefab;
+	public GameObject targetPlayer;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+//		Instantiate (myCube, transform.position, transform.rotation);
+
+		if (Input.GetKeyDown (KeyCode.M)) {
+			Instantiate (myZombiePrefab, targetPlayer.transform.position, targetPlayer.transform.rotation);
+		}
+
+	}
+}
