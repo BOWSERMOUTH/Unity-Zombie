7bd384f187bd928d1cfe184950fb19d5a85f85d9
Jeffrey Meldgin <Jeff@Jeff-Medglins-JockBook-Bro.local>
2016-07-06 18:27:49 -0700

    wip add end game

---
+++ Assets/FinishScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishScript : MonoBehaviour {
+	AudioSource myAudio;
+
+	// Use this for initialization
+	void Start () {
+		myAudio = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (Collider collider in collision.collider) {
+			Debug.Log ("COLISSION DETECTED!!!");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PlaySpawnScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySpawnScript : MonoBehaviour {
+
+	public Transform spawnLocation;
+
+	// Use this for initialization
+	void Start () {
+		if (spawnLocation != null) {
+			transform.position = spawnLocation.position;
+		} else {
+			Debug.LogError ("No Spawn location");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
